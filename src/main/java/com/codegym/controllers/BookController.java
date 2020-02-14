3f87245623ca6ef34a92764385ddf46de533cda4
luongquytnvn <luongquytnvn@gmail.com>
2020-02-14 11:28:36 +0700

    first commit

--- src/main/java/com/codegym/controllers/BookController.java
+++ src/main/java/com/codegym/controllers/BookController.java
@@ -1,8 +1,10 @@
 package com.codegym.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class BookController {
 
