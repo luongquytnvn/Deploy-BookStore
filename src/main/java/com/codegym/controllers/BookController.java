109ae69ab3a7c8e05fd9a4e0d88a7c5817a7fd1b
luongquytnvn <luongquytnvn@gmail.com>
2020-02-14 12:40:27 +0700

    second commit

--- src/main/java/com/codegym/controllers/BookController.java
+++ src/main/java/com/codegym/controllers/BookController.java
@@ -1,15 +1,16 @@
 package com.codegym.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RestController
+@Controller
 public class BookController {
 
     @RequestMapping("/")
-    String index() {
+    public String index() {
         return "index";
     }
 }
