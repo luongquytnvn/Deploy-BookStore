e77f229975ac9429174338ce2b4639e7f0ff7fb5
luongquytnvn <luongquytnvn@gmail.com>
2020-02-14 09:23:56 +0700

    first commit

---
+++ src/main/java/com/codegym/BookStoreVudaiApplication.java
@@ -0,0 +1,32 @@
+package com.codegym;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class BookStoreVudaiApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BookStoreVudaiApplication.class, args);
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+}
